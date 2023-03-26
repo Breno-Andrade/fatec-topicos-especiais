6d891c4493f3aad2f88f2391c26fe0dda754b5c1
Breno Andrade <breno.silva15@fatec.sp.gov.br>
2023-03-26 13:12:18 -0300

    Refactor: modificacoes na pagina index do front-end

--- frontend-vanillajs/javascript/customer/index.js
+++ frontend-vanillajs/javascript/customer/index.js
@@ -1,12 +1,24 @@
 const customerBody = document.getElementById('tableBody')
+const backendBaseURL = 'http://localhost:8080/customers'
+
+async function deleteCustomer(itin) {
+    try {
+        await fetch(`${backendBaseURL}/${itin}`, {
+            method: 'delete'
+        })
+        window.location = "index.html"
+    } catch(error) {
+        console.error(error)
+    }
+}
 
 async function getCustomers() {
     try {
-        const response = await fetch(`http://localhost:8080/customers`)
+        const response = await fetch(backendBaseURL)
         const customers = await response.json()
         
         if(customers.length === 0) {
-            customerBody.innerHTML = `<tr><td colspan="4">No customers found</td></tr>`
+            customerBody.innerHTML = `<tr><td colspan="5">No customers found</td></tr>`
         } else {
             for(const customer of customers) {
                 customerBody.innerHTML = customerBody.innerHTML + `<tr>
@@ -14,10 +26,14 @@ async function getCustomers() {
                     <td>${customer.itin}</td>
                     <td>${customer.phone}</td>
                     <td>${customer.email}</td>
+                    <td>
+                    <a href="update.html?itin=${customer.itin}">Update</a>
+                    <a href="javascript:void(0)" onclick="deleteCustomer('${customer.itin}')">Delete</a>
+                    </td>
                 </tr>`
             }
             customerBody.innerHTML =
-                customerBody.innerHTML + `<tr><td colspan="4">${customers.length} customer(s) found.</td></tr>`
+                customerBody.innerHTML + `<tr><td colspan="5">${customers.length} customer(s) found.</td></tr>`
         }
         
     } catch(error) {
